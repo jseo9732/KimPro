bdd266c3ea8cf4a4223b657bb7fb318bc2e0d61a
jisu Seo <jseo9732@gmail.com>
2025-01-13 00:37:36 +0900

    Revert "Fix: 환율 한국수출입은행 Open API로 수정"
    This reverts commit c16195843c1f81fdb8b78d80e5596daa7cc23ab9.

--- src/hooks/binance/useFetchExchangeRate.ts
+++ src/hooks/binance/useFetchExchangeRate.ts
@@ -1,21 +1,40 @@
-import getExchangeRate from 'api/getExchangeRate';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useRecoilState } from 'recoil';
 import { exchangeRateState } from 'recoil/atoms/exchangeAtoms';
 
+interface IExchangeRate {
+  value: number;
+}
+
 export const useFetchExchangeRate = () => {
   const [exchangeRate, setExchangeRate] = useRecoilState(exchangeRateState);
+  const eventSource = useRef<EventSource | null>(null);
+
+  const SSE_URL = process.env.REACT_APP_FX_IDC_API_URL;
 
   useEffect(() => {
-    const fetchExchangeRate = async () => {
-      const usdExchangeRate = await getExchangeRate();
-      if (usdExchangeRate) {
-        setExchangeRate(usdExchangeRate);
-      }
-    };
+    if (SSE_URL) {
+      eventSource.current = new EventSource(SSE_URL);
 
-    fetchExchangeRate();
-  }, [setExchangeRate]);
+      eventSource.current.onmessage = (event) => {
+        const json: IExchangeRate = JSON.parse(event.data);
+
+        if (json.value) {
+          const rate = json.value;
+          setExchangeRate(rate);
+        }
+      };
+
+      eventSource.current.onerror = (err) => {
+        console.error('Server-Sent Events Error:', err);
+        eventSource.current?.close();
+      };
+    }
+
+    return () => {
+      eventSource.current?.close();
+    };
+  }, []);
 
   return { exchangeRate };
 };
